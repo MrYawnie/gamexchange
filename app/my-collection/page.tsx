669dc75b74be17115e1b3883f8250f8cbad86ceb
MrYawnie <jani.andsten@gmail.com>
2024-11-26 12:27:59 +0200

    fix collection not being pushed to bottom

--- app/my-collection/page.tsx
+++ app/my-collection/page.tsx
@@ -3,8 +3,8 @@ import GameLibrary from "@/components/server/gamelist-server";
 export default async function Page() {
 
   return (
-    <div className="grid grid-rows-[1fr] items-center justify-items-center min-h-screen p-8 pb-10 gap-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[1fr] justify-items-center min-h-screen p-8 pb-10 gap-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center sm:items-start">
           <GameLibrary />
       </main >
     </div >
